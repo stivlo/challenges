27938f6d94246b01e7a837c850352b0fdd972ddf
Stefano Locati <stivlo@gmail.com>
2019-01-27 19:04:10 +0000

    Project setup & max depth of a binary tree solution

---
+++ src/trees/maximum_depth/Solution.java
@@ -0,0 +1,21 @@
+package trees.maximum_depth;
+
+import trees.model.TreeNode;
+
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
+ * 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
