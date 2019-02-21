156d50e5fffef1f46a22a24484d56b79a6dfe3af
Stefano Locati <stivlo@gmail.com>
2019-02-21 18:42:11 +0000

    Day 24 of 100: first bad version

---
+++ src/main/java/search/Solution.java
@@ -0,0 +1,39 @@
+package search;
+
+import java.util.Random;
+
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/774/
+public class Solution {
+
+    private int firstBadVersion;
+
+    public Solution(int firstBadVersion) {
+        this.firstBadVersion = firstBadVersion;
+    }
+
+    public int firstBadVersion(int n) {
+        long maxGood = 0;
+        long minBad = n + 1L;
+        while (minBad - maxGood > 1) {
+            long mid = (maxGood + minBad) / 2L;
+            if (isBadVersion((int) mid)) {
+                minBad = mid;
+            } else {
+                maxGood = mid;
+            }
+        }
+        return (int) minBad;
+    }
+
+    private boolean isBadVersion(int version) {
+        return version >= firstBadVersion;
+    }
+
+    public static void main(String[] args) {
+        int n = 2147483647;
+        int firstBadVersion= 2147483647;
+        System.out.println("found: " + new Solution(firstBadVersion).firstBadVersion(n));
+    }
+
+}
