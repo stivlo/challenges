17427320089d765c14da5ee977a3c53b60123a0c
Stefano Locati <stivlo@gmail.com>
2019-02-25 00:10:58 +0000

    Day 27 of 100 Days of Code: max special product correct solution

---
+++ src/main/java/arrays/maxsprod/CorrectSolution.java
@@ -0,0 +1,41 @@
+package arrays.maxsprod;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class CorrectSolution {
+
+    public int maxSpecialProduct(ArrayList<Integer> A) {
+        int n = A.size();
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Deque<Integer> q = new ArrayDeque<>();
+        q.addLast(0);
+
+        for (int i = 1; i < n; i++) {
+            processItem(A, left, q, i);
+        }
+        q = new ArrayDeque<>();
+        q.addLast(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            processItem(A, right, q, i);
+        }
+        long mx = -1;
+        for (int i = 0; i < n; i++) {
+            mx = Long.max(mx, (long) left[i] * right[i]);
+        }
+        return (int) (mx % 1000000007);
+    }
+
+    private void processItem(ArrayList<Integer> A, int[] left, Deque<Integer> q, int i) {
+        while (!q.isEmpty()) {
+            if (A.get(q.getLast()) > A.get(i)) break;
+            q.removeLast();
+        }
+        left[i] = (q.isEmpty()) ? 0 : q.getLast();
+        q.addLast(i);
+    }
+
+}
