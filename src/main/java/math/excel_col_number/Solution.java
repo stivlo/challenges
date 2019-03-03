05910fd5fde55f34317c0abe2c0fc978df3050d4
Stefano Locati <stivlo@gmail.com>
2019-03-03 23:33:25 +0000

    Day 34 of 100 Days of Code: Excel column number

---
+++ src/main/java/math/excel_col_number/Solution.java
@@ -0,0 +1,18 @@
+package math.excel_col_number;
+
+// https://www.interviewbit.com/problems/excel-column-number/
+public class Solution {
+
+    public int titleToNumber(String a) {
+        int base = 26;
+        int mult = 1;
+        int result = 0;
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int digit = ((a.charAt(i) - 'A') + 1);
+            result += digit * mult;
+            mult *= base;
+        }
+        return result;
+    }
+
+}
